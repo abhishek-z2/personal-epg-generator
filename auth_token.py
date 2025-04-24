3998269bc6344086c33b9de71d56986f26e6d7a4
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2025-04-24 11:34:56 +0530

    Add dynamic params and Proper Header

--- auth_token.py
+++ auth_token.py
@@ -1,8 +1,10 @@
 import requests
-import os
+from datetime import datetime
+import uuid
 
-def get_pluto_auth_token():
-    url = "https://boot.pluto.tv/v4/start"
+def get_pluto_session_token(session):
+    now = datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
+    
     params = {
         "appName": "web",
         "appVersion": "9.11.0-fd026e7e9714f1e273c3c58069eb9e967f2c7ca8",
@@ -10,32 +12,43 @@ def get_pluto_auth_token():
         "deviceModel": "web",
         "deviceMake": "firefox",
         "deviceType": "web",
-        "clientID": "196afddd-979a-46b8-b248-34100e5e0b74",
+        "clientID": str(uuid.uuid4()),
         "clientModelNumber": "1.0.0",
-        "channelID": "62ba60f059624e000781c436",
+        "channelID": "5f4d8594eb979c0007706de7",  
         "serverSideAds": "false",
         "blockingMode": "",
         "notificationVersion": "1",
         "appLaunchCount": "0",
-        "lastAppLaunchDate": "2025-04-23T18%3A38%3A28.305Z",
-        "clientTime": "2025-04-23T18%3A44%3A17.845Z",
+        "lastAppLaunchDate": now,
+        "clientTime": now,
     }
 
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    
-    # Retrieve sessionToken
-    session_token = response.json().get("sessionToken")
-    if not session_token:
-        raise Exception("No sessionToken found in the response.")
-    
-    return session_token
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Host": "boot.pluto.tv",
+        "Origin": "https://pluto.tv",
+        "Pragma": "no-cache",
+        "Referer": "https://pluto.tv/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-GPC": "1",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"
+    }
 
-def save_token():
-    token = get_pluto_auth_token()
-    with open("token.txt", "w") as f:
-        f.write(token)
-    print("Token saved successfully.")
+    response = session.get("https://boot.pluto.tv/v4/start", params=params, headers=headers)
 
-if __name__ == "__main__":
-    save_token()
+
+    if response.status_code == 200:
+        print(response.text)
+        data = response.json()
+        return data.get("sessionToken")
+    else:
+        print(f"Failed to get token. Status code: {response.status_code}")
+        return None
+# print()
+# print(get_pluto_session_token())
