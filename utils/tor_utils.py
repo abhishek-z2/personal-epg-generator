51bcd71f3eae3f74e029987e6d43c903c6c0e523
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2025-04-29 21:45:06 +0530

    Update port number

--- utils/tor_utils.py
+++ utils/tor_utils.py
@@ -1,11 +1,21 @@
 import requests
+import os
+TOR_PORT = os.getenv("TOR_PORT", "9050")  # default to 9050 if not set
 
 def get_tor_session():
     session = requests.Session()
+
+
     session.proxies = {
-        'http': 'socks5h://127.0.0.1:9050',
-        'https': 'socks5h://127.0.0.1:9050'
+        'http': f'socks5h://127.0.0.1:{TOR_PORT}',
+        'https': f'socks5h://127.0.0.1:{TOR_PORT}'
     }
+
+
+    # session.proxies = {
+    #     'http': 'socks5h://127.0.0.1:9050',
+    #     'https': 'socks5h://127.0.0.1:9050'
+    # }
     ipinfo = session.get("http://ip-api.com/json/").json()
     print(ipinfo)
     return session
